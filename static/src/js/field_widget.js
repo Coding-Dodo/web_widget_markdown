11f9e70b3c32ff08b3d2a9984d3594b5542a2ecd
PhilDL <philippe.lattention@hotmail.fr>
2021-03-31 10:33:40 +0400

    [FIX] Adding lib folder

--- static/src/js/field_widget.js
+++ static/src/js/field_widget.js
@@ -1,15 +1,82 @@
-odoo.define('web_widget_markdown', function (require) {
+odoo.define('my_field_widget', function (require) {
 "use strict";
+
+var AbstractField = require('web.AbstractField');
 var fieldRegistry = require('web.field_registry');
-var basicFields = require('web.basic_fields');
 
 
-var markdownField = basicFields.FieldText.extend({
+var markdownField = AbstractField.extend({
     supportedFieldTypes: ['text'],
-    className: 'o_field_markdown',
+    template: 'FieldMarkdown',
+    events: {}, // events are triggered manually for this debounced widget
     jsLibs: [
-        '/web_widget_markdown/static/lib/simplemde.min.js',
+        '/my_library/static/lib/simplemde.min.js',
     ],
+
+    /**
+     * @constructor
+     */
+    init: function () {
+        this._super.apply(this, arguments);
+        this.simplemde = {}
+    },
+
+    /**
+     * As it it done in the start function, the autoresize is done only once.
+     *
+     * @override
+     */
+    start: function () {
+        var self = this;
+        if (this.mode === 'edit') {
+            var $textarea = this.$el.find('textarea');
+            var simpleMdeConfig = {
+                element: $textarea[0], 
+                initialValue: this.value,
+                placeholder: "Type here...",
+                autofocus: false,
+                renderingConfig: {
+                    codeSyntaxHighlighting: true,
+                },
+            }
+            if(this.nodeOptions.autosave) {
+                simpleMdeConfig.autosave = {
+                    enabled: true,
+                    uniqueId: "markdown-"+this.model+this.res_id,
+                    delay: 1000
+                };
+            }
+            if(this.nodeOptions.placeholder) {
+                simpleMdeConfig.placeholder = this.nodeOptions.placeholder;
+            }
+            
+            this.simplemde = new SimpleMDE(simpleMdeConfig);
+            this.simplemde.codemirror.on("change", function(){
+                self._setValue(self.simplemde.value());
+            })
+            // If inehrit basicFields.DebouncedField
+            // this.simplemde.codemirror.on("change", this._doDebouncedAction.bind(this))
+            self._setValue(self.simplemde.value());
+        }
+        return this._super();
+    },
+
+    /**
+     * @override destroy from AbstractField (Widget)
+     */
+    destroy: function () {
+        this.simplemde = null;
+        this._super.apply(this, arguments);
+    },
+
+    /**
+     * return the SimpleMDE value
+     *
+     * @private
+     */
+    _getValue: function () {
+        return this.simplemde.value();
+    },
 
     _renderReadonly: function () {
         this.$el.html(SimpleMDE.prototype.markdown(this.value));
